96d6bf5e2e1e259c5813a2a926255f80e879f030
Leandro Andrade <leandroandrade020797@gmail.com>
2025-04-28 10:53:26 -0300

    Create exercicio04_dissecando_variavel.pyexercicio04_dissecando_variavel.py

---
+++ Aula06_Tipos_Primitivos/exercicio04_dissecando_variavel.pyexercicio04_dissecando_variavel.py
@@ -0,0 +1,38 @@
+n = input("Digite algo: ")
+print(type(n))
+
+
+n = input("Digite algo: ")
+print(n.isalpha())
+
+n = input("Digite algo: ")
+print(n.islower())
+
+n = input("Digite algo: ")
+print(n.isdecimal())
+
+n = input("Digite algo: ")
+print(n.isspace())
+
+n = input("Digite algo: ")
+print(n.istitle())
+
+n = input("Digite algo: ")
+print(n.islower())
+
+
+
+n1 = input("Digite um número: ")
+n2 = input("Digite outro número: ")
+s = n1 + n2
+print(s.isnumeric())
+
+n1 = input("Digite um número: ")
+n2 = input("Digite outro número: ")
+s = n1 + n2
+print(type(s))
+
+n1 = int(input("Digite um número: "))
+n2 = int(input("Digite outro número: "))
+s = n1 + n2
+print(type(s))
