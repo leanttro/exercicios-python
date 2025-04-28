9436431d91cb22c7c33cf5226f400f136318c6ef
Leandro Andrade <leandroandrade020797@gmail.com>
2025-04-28 12:10:04 -0300

    Aula07_Operadores_Aritmeticos

---
+++ Aula07_Operadores_Aritmeticos/Aula07_Operadores_Aritmeticos.py
@@ -0,0 +1,39 @@
+n1 = int(input("Digite um número: "))
+n2 = int(input("Digite outro número: "))
+s = n1 + n2
+mult = n1 * n2
+d = n1 / n2
+dint = n1 // n2
+exp = n1 ** n2
+
+print("A soma é {}, \n o produto é {}, \na divisão é {:.2f} ".format(s, mult ,d),end="")
+print("divisão inteira é {}, expoente é {}".format(dint, exp))
+
+n1 = int(input("Digite um número: "))
+n2 = int(input("Digite outro número: "))
+s = n1 + n2
+mult = n1 * n2
+d = n1 / n2
+dint = n1 // n2
+exp = n1 ** n2
+
+print("A soma é {}, o produto é {}, a divisão é {}".format(s, mult ,d))
+print("Divisão inteira é {}, expoente é {}".format(dint, exp))
+
+
+
+n1 = input("Digite um número: ")
+n2 = input("Digite outro número: ")
+print("A some é {} ".format(n1+n2))
+
+nome=input("Qual o seu nome? ")
+print("Prazer em te conhecer {:^20}!".format(nome))
+
+nome=input("Qual o seu nome? ")
+print("Prazer em te conhecer {:=^20}!".format(nome))
+
+nome=input("Qual o seu nome? ")
+print("Prazer em te conhecer {:>20}!".format(nome))
+
+nome=input("Qual o seu nome? ")
+print("Prazer em te conhecer {:<20}!".format(nome))
