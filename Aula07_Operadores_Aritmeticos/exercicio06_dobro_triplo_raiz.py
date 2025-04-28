9c41f623babd50877ae7db4dc550e2be7c8612a8
Leandro Andrade <leandroandrade020797@gmail.com>
2025-04-28 16:12:47 -0300

    commit

---
+++ Aula07_Operadores_Aritmeticos/exercicio06_dobro_triplo_raiz.py
@@ -0,0 +1,5 @@
+n = int(input("Digite um número: "))
+n1 = n * 2
+n2 = n * 3
+n3 = n ** 0.5
+print("O número escolhido foi {}. \nO seu dobro é {}. \nO seu triplo é {}. \nA sua raiz quandrada é {:.2f}.".format(n,n1,n2,n3))
