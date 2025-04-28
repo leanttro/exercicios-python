fb93c94560be1c96e0ef6d7106758dd061aa72cd
Leandro Andrade <leandroandrade020797@gmail.com>
2025-04-28 10:29:14 -0300

    aula06

---
+++ Aula06_Tipos_Primitivos/aula06.py
@@ -0,0 +1,22 @@
+n1 =(input('Digite um valor: '))
+n2 =(input('Digite outro valor: '))
+s = n1 + n2
+print('A soma é ', s)
+
+# Sem definir o tipo primitivo de variável, o python lê string automaticamente
+
+n1 =int(input('Digite um valor: '))
+n2 =int(input('Digite outro valor: '))
+s = n1 + n2
+print('A soma é ', s)
+
+# Ao definir o tipo primitivo de variável para int, o python lê números inteiros
+
+n1 =int(input('Digite um valor: '))
+n2 =int(input('Digite outro valor: '))
+s = n1 + n2
+# print('A soma entre ', n1, "e ", n2, "vale ", s)
+print("A soma entre {} e {} é {}".format(n1, n2, s))
+
+
+
